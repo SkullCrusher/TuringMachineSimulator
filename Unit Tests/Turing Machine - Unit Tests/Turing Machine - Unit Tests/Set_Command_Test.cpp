d5e5bbcc1c28383581f187444f84ab05c5b3894e
David M Harkins <boxhead.two.play@gmail.com>
2016-11-10 17:36:53 -0800

    Finished the set command

--- Unit Tests/Turing Machine - Unit Tests/Turing Machine - Unit Tests/Set_Command_Test.cpp
+++ Unit Tests/Turing Machine - Unit Tests/Turing Machine - Unit Tests/Set_Command_Test.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
+#include "Set_Command.h"
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -18,6 +20,23 @@ namespace TuringMachineUnitTests
 			// Fake user input ;)
 			std::istringstream iss("1\nds1\n");
 			std::cin.rdbuf(iss.rdbuf());
+
+			std::stringstream buffer;
+			std::streambuf * old;
+
+			int Transition_Max = 0;
+			old = std::cout.rdbuf(buffer.rdbuf());
+
+
+			Set_Command(Transition_Max);
+			std::string Result = buffer.str();
+
+			Assert::AreEqual(Result[0], 'M');
+
+			Set_Command(Transition_Max);
+			Result = buffer.str();
+
+			Assert::AreEqual(Result[106], 'E');
 
 		}
 
